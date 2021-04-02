4d2219c811f9c8e7b1bc72aded830fce9bfcd329
TRex-Dino <dmitrysinkov6@gmail.com>
2021-04-02 11:26:06 +0300

    edit log in view

--- ASPushkin2.4/ViewController.swift
+++ ASPushkin2.4/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func logInPressed() {
+    }
+    
+    @IBAction func forgoData() {
+    }
 }
 
